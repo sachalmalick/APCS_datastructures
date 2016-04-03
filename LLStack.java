c57a40c218a402737fd96880a3576fa636ed6527
Kyle Moon <kmoon@stuy.edu>
2016-04-02 22:14:57 -0400

    added headings, just an initial commit

--- LLStack.java
+++ LLStack.java
@@ -1,3 +1,10 @@
+/*
+  Team: Over the Moon -- Sachal Malick, Kyle Moon
+  APCS2 PD10
+  HW24 -- Schemin
+  2016-02-2016
+*/
+
 /*****************************************************
  * class LLStack
  * Implements ADT stack as specified in local Stack.java
@@ -10,100 +17,100 @@ import java.util.LinkedList;
 
 public class LLStack<T> implements Stack<T> {
 
-	LinkedList <T> _stack;
+    LinkedList <T> _stack;
     private int _stackSize;
-    
- 
-	public LLStack() {
-		_stack = new LinkedList<T>();
-		_stackSize = 0;
-	}
 
-	@Override
-	public boolean isEmpty() {
-		
-		return _stackSize == 0; 
-	}
 
-	@Override
-	public T peek() {
-		if (isEmpty()) return null;
-		return _stack.get(_stackSize-1);
-	}
+    public LLStack() {
+        _stack = new LinkedList<T>();
+        _stackSize = 0;
+    }
 
-	@Override
-	public T pop() {
-		if (isEmpty()) return null;
-		T temp = peek();
-		_stack.remove(_stackSize-1);
-		_stackSize--;
-		return temp;
-	}
+    @Override
+    public boolean isEmpty() {
 
-	@Override
-	public void push(T x) {
-		_stack.add(x);
-		_stackSize++;
-	}
+        return _stackSize == 0;
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) return null;
+        return _stack.get(_stackSize-1);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) return null;
+        T temp = peek();
+        _stack.remove(_stackSize-1);
+        _stackSize--;
+        return temp;
+    }
+
+    @Override
+    public void push(T x) {
+        _stack.add(x);
+        _stackSize++;
+    }
 
     //main method for testing
     public static void main( String[] args ) {
 
-	Stack<String> tastyStack = new LLStack<String>();
+        Stack<String> tastyStack = new LLStack<String>();
 
-	tastyStack.push("aoo");
-	tastyStack.push("coo");
-	tastyStack.push("doo");
-	tastyStack.push("eoo");
-	tastyStack.push("foo");
-	tastyStack.push("goo");
-	tastyStack.push("hoo");
-	tastyStack.push("ioo");
-	tastyStack.push("joo");
-	tastyStack.push("coocoo");
-	tastyStack.push("cachoo");
+        tastyStack.push("aoo");
+        tastyStack.push("coo");
+        tastyStack.push("doo");
+        tastyStack.push("eoo");
+        tastyStack.push("foo");
+        tastyStack.push("goo");
+        tastyStack.push("hoo");
+        tastyStack.push("ioo");
+        tastyStack.push("joo");
+        tastyStack.push("coocoo");
+        tastyStack.push("cachoo");
 
-	//cachoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//coocoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//joo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//ioo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//hoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//goo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//foo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//eoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//doo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//coo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//boo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	//aoo
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
+        //cachoo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //coocoo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //joo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //ioo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //hoo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //goo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //foo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //eoo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //doo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //coo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //boo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        //aoo
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
 
-	//stack empty by now; SOP(null)
-	System.out.println( "peek: " + tastyStack.peek() );
-	System.out.println( "pop: " + tastyStack.pop() );
-	System.out.println( tastyStack.pop() );
+        //stack empty by now; SOP(null)
+        System.out.println( "peek: " + tastyStack.peek() );
+        System.out.println( "pop: " + tastyStack.pop() );
+        System.out.println( tastyStack.pop() );
     }//end main()
 
 
